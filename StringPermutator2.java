0806eac7d55a476909e94f4cec5819c301fa64c2
loay-mustafa <73184724+loay-mustafa@users.noreply.github.com>
2021-01-18 23:25:41 +0200

    Update StringPermutator2.java

--- StringPermutator2.java
+++ StringPermutator2.java
@@ -3,9 +3,14 @@
 /* the program print number of permution after print them all */
 /*we can divide function to find factorial But to facilitate the reading and understanding of the program I did not divide it*/
 /* the program print all permutation we can improve it to print permutation without dublicate*/ 
-import java.util.Scanner;
+
+import java.util.*;
+
 public class StringPermutator2 {
-    private static void PrintPermutationsIterative(String string){
+    
+    private static List<String> PrintPermutationsIterative(String string){
+        List<String> list=new ArrayList<String>();
+
      int [] factorials = new int[string.length()+1];  //Array  to handle factorial of integers
         factorials[0] = 1;
         for (int i = 1; i<=string.length();i++) {
@@ -24,15 +29,30 @@ public class StringPermutator2 {
                 positionCode = positionCode % factorials[position-1];
                 temporary = temporary.substring(0,selected) + temporary.substring(selected+1);
             }
-            System.out.println(onePermutation);      // print permutation
+             list.add(onePermutation);     // print permutation
 
         }
             System.out.println("number of permutation is "+ factorials[string.length()]);  // // print the number permutation
-  
+  return list;
 
     }
+    static boolean cheakPermutations(List<String> list, String string){
+             char ch1[] = string.toCharArray(); // convert input string to character array
+             Arrays.sort(ch1);                  //sort input string character array
+for (int i = 0; i < list.size(); i++){         // loop for Extract Permutation from the list
+
+    char ch2[]=list.get(i).toCharArray();        // convert Permutation to character array
+    Arrays.sort(ch2);                            //sort Permutation character array
+    for (int j = 0; j<ch1.length; j++){           // loop for cheak if Permutation equal the input string
+    if (ch1[j] != ch2[j]) {                 
+        return false;}                               // return false if there uncorrect Permutation
+  
+    } }
+       return true;                                // true if all Permutation True
+     }
 public static void main(String[] args) {
  Scanner scanner = new Scanner(System.in);
+ List<String> list=new ArrayList<String>();
   while (true){       // loop stoped when user input string 
      System.out.print("Enter value to permutate: ");
      String string = scanner.nextLine();
@@ -40,7 +60,11 @@ public static void main(String[] args) {
      System.out.println("erorr, you must input  value to permutate  ");
 }
       else {
-        PrintPermutationsIterative(string);     // Call a function to find Permutations
+          
+        list=PrintPermutationsIterative(string); 
+            System.out.println(list.toString());
+            System.out.println(cheakPermutations(list, string));
+        // Call a function to find Permutations
          scanner.close();
          break;
 }}}}
